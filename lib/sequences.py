79b00f9fb1b649092f255d85ff97c2daa2dca2f6
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-08-19 12:14:59 +0300

    Fibonacci sequence Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    array = list()
+
+    if length == 0:
+        print(array)
+    elif length == 1:
+        array.append(0)
+        print(array)
+    else:
+        sequence = [0, 1]
+        for i in range(2, length):
+            next_fib = sequence[i - 1] + sequence[i - 2]
+            sequence.append(next_fib)
+        print(sequence)
+
+
+print_fibonacci(9)
+print_fibonacci(20)
